bdb7e334a0058c6e0f11b1eb248c86f8323044cc
greysmpich <greys.mpich@gmail.com>
2024-03-14 21:38:57 -0600

    chore: updated

--- server.js
+++ server.js
@@ -14,7 +14,13 @@ const corsOptions = {
     optionsSuccessStatus: 204,
   };
 
-app.use(cors(corsOptions));
+app.use(cors(corsOptions)).use((req, res, next) => {
+  res.header('Access-Control-Allow-Origin', '*');
+  res.header('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE');
+  res.header('Access-Control-Allow-Headers', 'Content-Type');
+  next();
+});
+
 const router = jsonServer.router(path.join(__dirname, 'db.json'));
 
 const port = process.env.PORT || 8080;
